60b0a8b0a0a0ab96f3f7febdedb03a38d2fb5100
Karol Gozdzikowski <137559533+GoKarol@users.noreply.github.com>
2024-03-31 06:40:51 +0100

    Update member.js

--- member.js
+++ member.js
@@ -1,1 +1,13 @@
-function skillsMember()
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'moduless/skills/views;member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'SkillsMemberController',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };  
+    // code goes here
+}
