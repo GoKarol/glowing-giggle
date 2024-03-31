f8a0f049643315ae75b57a4e8ffd5c5bfe58a07f
Karol Gozdzikowski <137559533+GoKarol@users.noreply.github.com>
2024-03-31 06:10:41 +0100

    Update skills.js

--- skills.js
+++ skills.js
@@ -1,3 +1,9 @@
 function calculateNumbers(var1, var2){
   console.log(var1 + var2);
 }
+function multiplyNumbers(var1, var2){
+  // code for multiplying numbers
+}
+function divideNumbers(var1, var2){
+  // code for dividing numbers
+}
